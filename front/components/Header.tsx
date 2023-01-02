1791c1b881d214ff59b905050a265c2bf9ea80d6
always-reach <67514209+always-reach@users.noreply.github.com>
2023-01-02 21:57:42 +0900

    add header

---
+++ front/components/Header.tsx
@@ -0,0 +1,16 @@
+import * as React from "react"
+
+const Header = () => {
+    return (
+        <nav className="flex items-center justify-between flex-wrap bg-teal-500 p-6">
+            <div className="flex items-center flex-shrink-0 text-white mr-6">
+                <span className="font-semibold text-xl tracking-tight">Solo Duel</span>
+            </div>
+            <div className="grow flex items-center w-auto">
+                
+            </div>
+        </nav>
+    )
+}
+
+export default Header
