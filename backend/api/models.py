b497d112238c5bf91af5709ad8c72ffcc5c6e40a
always-reach <67514209+always-reach@users.noreply.github.com>
2023-01-06 20:33:36 +0900

    add user model

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin, UserManager
+from django.utils.translation import gettext_lazy as _
+
 
 # Create your models here.
+
+class User(AbstractBaseUser, PermissionsMixin):
+    user_name = models.CharField(verbose_name="ユーザー名", blank=False, null=False, max_length=255, unique=True)
+    email = models.EmailField(verbose_name="メールアドレス", blank=False, null=False, unique=True)
+    password = models.CharField(verbose_name="パスワード", blank=False, null=False, max_length=255)
+    last_login = models.DateTimeField(verbose_name="最終ログイン", null=True, blank=True)
+    is_staff = models.BooleanField(verbose_name="スタッフ権限", default=True)
+    is_active = models.BooleanField(verbose_name="論理削除", default=True)
+    is_superuser = models.BooleanField(verbose_name="スーパーユーザー", default=False)
+
+    objects = UserManager()
+    EMAIL_FIELD = "email"
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["user_name", "password"]
+
+    class META:
+        verbose_name = _("user")
+        verbose_name_plural = _("users")
+
+    def __str__(self):
+        return str(self.__dict__)
+
+    def get_full_name(self):
+        return self.user_name
+
+    def has_module_perms(self, app_label):
+        return True
+
+    def has_perm(self, perm, obj=None):
+        return True
+
