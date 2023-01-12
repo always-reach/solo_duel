c2a5de1d5a0e6c579d671c6b76ce85cb3e07eaa2
always-reach <67514209+always-reach@users.noreply.github.com>
2023-01-12 23:23:18 +0900

    cssが変

--- front/app/deck/page.tsx
+++ front/app/deck/page.tsx
@@ -1,9 +1,19 @@
 import TextField from "../../components/TextField"
 
-const CreateDeck:React.FC=()=>{
-    return(
+const CreateDeck: React.FC = () => {
+    return (
         <div>
-            <TextField placeholder="デッキ名"/>
+            <TextField placeholder="デッキ名" />
+            <div className="grid grid-cols-9">
+                <div className="bg-blue-300 border-2 rounded-lg col-span-1">
+                    デッキの中身
+                </div>
+                <div className="bg-blue-300 border-2 rounded-lg col-span-1">
+                    カードリスト
+                </div>
+            </div>
+            
+
         </div>
     )
 }
